7278b42f9f65b90122225544ba12116e8000f3d1
Liem Nguyen <liemnguyen400@gmail.com>
2020-12-12 13:42:20 -0500

    testing git pull part 2

--- src/Emailform.js
+++ src/Emailform.js
@@ -19,22 +19,6 @@ export function Emailform(props) {
     return (
       <Thanks />
     );
-  } else if (isCreated) {
-    return (
-      <div className="emailform-root">
-        <div className="emailform-heading">
-          <h1>Code Time Capsule!</h1>
-          <h4>Welcome! Here, you can type or paste a sample of your code and then we email you a copy of it sometime up to 10 years later so that you can see how you’ve grown. </h4>
-        </div>
-        <Form />
-        <div className="code-view">
-          <h1>Here's your most recently made message! </h1>
-          <h4>(This is currently view-only and formatting is not preserved yet.)</h4>
-          <button className="view-recent-button" onClick={onLoad}>Load Message</button>
-          <textarea placeholder="Click the button above to load in your message">{code}</textarea>
-        </div>
-      </div>
-    );
   } else {
     return (
       <div className="emailform-root">
